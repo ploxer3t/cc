a2ce298e00042eaf296a79165812a9bf0911e511
Ploxeret <140412334+ploxer3t@users.noreply.github.com>
2023-10-04 20:34:21 +0300

    Update cc.py

--- cc.py
+++ cc.py
@@ -1,8 +1,8 @@
 import random
 import time
 
-def generate_credit_card_number():
-    prefix = str(random.randint(4000000000000000, 7999999999999999))
+def generate_mastercard_number():
+    prefix = str(random.randint(5100000000000000, 5599999999999999))
     suffix = str(random.randint(100000000, 9999999999))
     return prefix + suffix
 
@@ -12,19 +12,17 @@ def generate_cvc():
 def generate_date(month, year):
     return f"{month}/{year}"
 
-def generate_credit_card_info(count):
+def generate_mastercard_info(count):
     for i in range(count):
-        credit_card_number = generate_credit_card_number()
+        mastercard_number = generate_mastercard_number()
         cvc = generate_cvc()
         date = generate_date(random.randint(1, 12), random.randint(2022, 2030))
-        bank = random.choice(["VISA", "MASTERCARD"])
 
-        print(f"\033[91mKredi Kartı Numarası: \033[32m{credit_card_number}\033[0m")
+        print(f"\033[91mKredi Kartı Numarası: \033[32m{mastercard_number}\033[0m")
         print(f"\033[91mCVC: \033[32m{cvc}\033[0m")
         print(f"\033[91mSon Kullanma Tarihi: \033[32m{date}\033[0m")
-        print(f"\033[91mBanka: \033[32m{bank}\033[0m")
         print(f"\033[94m----------------PLOXERET BİR DÜNYA MARKASI XDD----------------\033[0m \033")
         time.sleep(1)
 
-count = int(input("Kaç adet kredi kartı bilgisi üretmek istiyorsunuz?: "))
-generate_credit_card_info(count)
+count = int(input("Kaç adet CivCiv üretmek istiyorsunuz?: "))
+generate_mastercard_info(count)
